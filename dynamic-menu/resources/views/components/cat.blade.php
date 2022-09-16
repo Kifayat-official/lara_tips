6ea9231c3c38470ee971b8c35f43ee03887be822
Kifayat Ullah <kifayat.1234u@gmail.com>
2022-09-16 16:31:23 +0500

    changes

--- dynamic-menu/resources/views/components/cat.blade.php
+++
@@ -1,3 +0,0 @@
-<li class="parent"><a href="#">item <span class="expand">»</span></a></li>
-
-{{-- {{ $category->name }} --}}
--- dynamic-menu/routes/web.php
+++ dynamic-menu/routes/web.php
@@ -24,6 +24,3 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
