5971003e8024f66cc6b3f6439fabad7929e4fb31
Kifayat Ullah <kifayat.1234u@gmail.com>
2022-09-23 16:08:02 +0500

    changes

--- dynamic-menu/resources/views/welcome.blade.php
+++ dynamic-menu/resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- @vite('resources/css/app.css') --}}
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
         integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -14,7 +15,8 @@
 </head>
 
 <body>
-
+    {{-- {{ dd($categories) }} --}}
+    {{-- Dynamic Mega Menu --}}
     <div class="navbar">
 
         @foreach ($categories as $category)
@@ -31,7 +33,6 @@
                                     @foreach ($child->children as $child)
                                         <a href="#">{{ $child->name }}</a>
                                     @endforeach
-                                    {{-- <x-category :category="$child" /> --}}
                                 </div>
                             @endforeach
                         </div>
@@ -42,6 +43,105 @@
 
     </div>
 
+    {{-- Dynamic Heirarchical Tree  --}}
+    <div class="container" style="margin-top:30px;">
+        <div class="row">
+            <div class="col-md-4">
+                <ul id="tree1">
+                    <p class="well" style="height:135px;"><strong>Initialization no parameters</strong>
+
+                        <br /> <code>$('#tree1').treed();</code>
+
+                    </p>
+                    <li><a href="#">TECH</a>
+
+                        <ul>
+                            <li>Company Maintenance</li>
+                            <li>Employees
+                                <ul>
+                                    <li>Reports
+                                        <ul>
+                                            <li>Report1</li>
+                                            <li>Report2</li>
+                                            <li>Report3</li>
+                                        </ul>
+                                    </li>
+                                    <li>Employee Maint.</li>
+                                </ul>
+                            </li>
+                            <li>Human Resources</li>
+                        </ul>
+                    </li>
+                    <li>XRP
+                        <ul>
+                            <li>Company Maintenance</li>
+                            <li>Employees
+                                <ul>
+                                    <li>Reports
+                                        <ul>
+                                            <li>Report1</li>
+                                            <li>Report2</li>
+                                            <li>Report3</li>
+                                        </ul>
+                                    </li>
+                                    <li>Employee Maint.</li>
+                                </ul>
+                            </li>
+                            <li>Human Resources</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <p class="well" style="height:135px;"><strong>Initialization optional parameters</strong>
+
+                    <br /> <code>$('#tree2').treed({openedClass : 'glyphicon-folder-open', closedClass :
+                        'glyphicon-folder-close'});</code>
+
+                </p>
+                <ul id="tree2">
+                    <li><a href="#">TECH</a>
+
+                        <ul>
+                            <li>Company Maintenance</li>
+                            <li>Employees
+                                <ul>
+                                    <li>Reports
+                                        <ul>
+                                            <li>Report1</li>
+                                            <li>Report2</li>
+                                            <li>Report3</li>
+                                        </ul>
+                                    </li>
+                                    <li>Employee Maint.</li>
+                                </ul>
+                            </li>
+                            <li>Human Resources</li>
+                        </ul>
+                    </li>
+                    <li>XRP
+                        <ul>
+                            <li>Company Maintenance</li>
+                            <li>Employees
+                                <ul>
+                                    <li>Reports
+                                        <ul>
+                                            <li>Report1</li>
+                                            <li>Report2</li>
+                                            <li>Report3</li>
+                                        </ul>
+                                    </li>
+                                    <li>Employee Maint.</li>
+                                </ul>
+                            </li>
+                            <li>Human Resources</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
 
 
 </body>
