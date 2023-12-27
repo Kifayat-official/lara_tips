011c49949d52cbd58a01ed8e6e9d2a07da3b9192
Kifayat Ullah <kifayat.1234u@gmail.com>
2023-12-27 16:21:14 +0500

    changes

---
+++ node/e-shop/frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+
+import Login from '../components/auth/Login.jsx'
+
+const LoginPage = () => {
+    retrun(
+        <div>
+            <Login/>
+        </div>
+    )
+}
+
+export default LoginPage
